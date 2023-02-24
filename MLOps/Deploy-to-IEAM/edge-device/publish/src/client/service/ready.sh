042badb29e73283ef53873aa66f89bb123a68d58
Sanjeev Gupta <sanjeev@ibm.com>
2023-02-24 14:28:18 -0800

    mlops wnlp Deploy-on-IEAM

---
+++ MLOps/Deploy-to-IEAM/edge-device/publish/src/client/service/ready.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# ***************************************************************** #
+# (C) Copyright IBM Corporation 2022.                               #
+#                                                                   #
+# The source code for this program is not published or otherwise    #
+# divested of its trade secrets, irrespective of what has been      #
+# deposited with the U.S. Copyright Office.                         #
+# ***************************************************************** #
+
+echo "WNLP Client ready."
+tail -f /dev/null
